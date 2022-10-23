9384a0100dafdf66aa9d40faa74d2858a03f4d22
Issiac-js <115911939+Issiac-js@users.noreply.github.com>
2022-10-22 21:37:19 -0700

    HTML and JavaScript folder added
    Assignment utilizing img and font elements

---
+++ Basic_HTML_and_JavaScript_1/Basic_JavaScript_1.js
@@ -0,0 +1,9 @@
+alert("Hello, World!");
+
+function My_First_Function() {
+
+  var string = "Kiss me im Irish";
+  var result = string.fontcolor("green");
+  document.getElementById("Irish").innerHTML = result;
+
+}
